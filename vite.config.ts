a931a1ac7e40451d04ebe877931f7e64b3a9564c
Oliver Ziemann <oliver@mordoor.de>
2025-01-01 17:20:37 +0100

    pod build

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths"; // --> import it
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react(), tsconfigPaths()],
+   build: {
+      outDir: 'docs' // Output directory for GitHub Pages
+   }
 });
