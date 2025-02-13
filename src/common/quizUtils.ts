c3757c57f0e2b13f70708d7d88c38790d34bca45
Oliver Ziemann <oliver.ziemann@maxqda.de>
2025-02-13 08:05:58 +0100

    🐛 Remove spaces at the end ob values, add settings.json to gitignore

--- src/common/quizUtils.ts
+++ src/common/quizUtils.ts
@@ -1,65 +1,82 @@
 export function isMoreThanADay(date1: Date, date2: Date) {
-    // Calculate the time difference in milliseconds
-    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+  // Calculate the time difference in milliseconds
+  const timeDiff = Math.abs(date2.getTime() - date1.getTime());
 
-    // Calculate the number of milliseconds in a day
-    const millisecondsPerDay = 48 * 60 * 60 * 1000;
+  // Calculate the number of milliseconds in a day
+  const millisecondsPerDay = 48 * 60 * 60 * 1000;
 
-    // Calculate the difference in days
-    const daysDiff = timeDiff / millisecondsPerDay;
+  // Calculate the difference in days
+  const daysDiff = timeDiff / millisecondsPerDay;
 
-    // Check if the difference is more than one day
-    return daysDiff > 1;
- }
+  // Check if the difference is more than one day
+  return daysDiff > 1;
+}
 
- 
- export function sameDate(date1: Date, date2: Date) {
-    return (
-       date1.getFullYear() === date2.getFullYear() &&
-       date1.getMonth() === date2.getMonth() &&
-       date1.getDate() === date2.getDate()
-    );
- }
+export function sameDate(date1: Date, date2: Date) {
+  return (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate()
+  );
+}
 
-    export function checkAgeGroup(value: string) {
-       if ("12-18" === value) return 1
-       if ("19-30" === value) return 2
-       if ("31-50" === value) return 3
-       if ("51-70" === value) return 4
-       if ("100+" === value) return 5
-    }
- 
-    export function getImgSrc(name: string) {
-       const filename = name.toLowerCase().replaceAll(" ", "_")
-       const basepath = !import.meta.env.PROD ? "/src/assets/characters/" : "assets/characters/"
- 
-       return basepath + filename + ".webp"
-    }
+export function checkAgeGroup(value: string) {
+  if ("12-18" === value) return 1;
+  if ("19-30" === value) return 2;
+  if ("31-50" === value) return 3;
+  if ("51-70" === value) return 4;
+  if ("100+" === value) return 5;
+}
 
-    export function compareObjects<T extends Record<string, any>>(obj1: T, obj2: T): {
-      all: string[],
-      short: string[]
-   } {
-      const sameFieldsObj: {
-         all: string[],
-         short: string[]
-      } = { all: [], short: [] };
-      const validFields = ["Name",
-         "Sex",
-         "Origin",
-         "Hair_Color",
-         "Age_Group",
-         "Height",
-         "Eye_Color", "First_Release_Year"]
+export function getImgSrc(name: string) {
+  const filename = name.toLowerCase().replaceAll(" ", "_");
+  const basepath = !import.meta.env.PROD
+    ? "/src/assets/characters/"
+    : "assets/characters/";
 
-      for (const key in obj1) {
-         if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key) && obj1[key] === obj2[key]) {
-            if (validFields.includes(key)) {
-               sameFieldsObj.short.push(key);
-            }
-            sameFieldsObj.all.push(key)
-         }
+  return basepath + filename + ".webp";
+}
+
+export function compareObjects<T extends Record<string, any>>(
+  obj1: T,
+  obj2: T
+): {
+  all: string[];
+  short: string[];
+} {
+  const sameFieldsObj: {
+    all: string[];
+    short: string[];
+  } = { all: [], short: [] };
+  const validFields = [
+    "Name",
+    "Sex",
+    "Origin",
+    "Hair_Color",
+    "Age_Group",
+    "Height",
+    "Eye_Color",
+    "First_Release_Year",
+  ];
+
+  for (const key in obj1) {
+    if (
+      obj1.hasOwnProperty(key) &&
+      obj2.hasOwnProperty(key) &&
+      obj1[key] === obj2[key]
+    ) {
+      if (typeof obj1[key] === "string" && obj1[key].endsWith(" ")) {
+        obj1[key] = obj1[key].slice(0, -1);
       }
+      if (typeof obj2[key] === "string" && obj2[key].endsWith(" ")) {
+        obj2[key] = obj2[key].slice(0, -1);
+      }
+      if (validFields.includes(key)) {
+        sameFieldsObj.short.push(key);
+      }
+      sameFieldsObj.all.push(key);
+    }
+  }
 
-      return sameFieldsObj;
-   }
+  return sameFieldsObj;
+}
--- src/components/BasicCharacterQuiz/CharacterList.tsx
+++ src/components/BasicCharacterQuiz/CharacterList.tsx
@@ -24,7 +24,7 @@ export default function CharacterList({ searchHistory, targetChar }: CharacterLi
 
    return (
       <Box>
-         <Box sx={{ marginTop: 4, display: "flex", flexDirection: "column", justifyContent: "flex-start", maxHeight: "400px", overflowX: "hidden", overflowY: "auto", borderTopLeftRadius: "16px", borderTopRightRadius: "16px", border: `1px solid ${COLORS.quiz.light}`, borderBottom: 0, paddingBottom: 2, backgroundColor: COLORS.quiz.secondary }}>
+         <Box sx={{ marginTop: 4, display: "flex", flexDirection: "column", justifyContent: "flex-start", maxHeight: "400px", overflowX: "hidden", overflowY: "auto", borderTopLeftRadius: "16px", borderTopRightRadius: "16px", border: `1px solid ${COLORS.quiz.light}`, borderBottom: 0, backgroundColor: COLORS.quiz.secondary }}>
             <Box sx={{ display: 'grid', gridTemplateColumns: '60px repeat(8, 1fr)', gap: 2, paddingX: 2 }}> {/* 4 equal columns */}
                {/* Header Row */}
                <Box sx={{ gridColumn: '1 / 2', textAlign: 'center', marginY: 2, fontWeight: "bold", color: COLORS.quiz.primary_text }}>Image</Box>
@@ -37,56 +37,56 @@ export default function CharacterList({ searchHistory, targetChar }: CharacterLi
                <Box sx={{ gridColumn: '8 / 9', textAlign: 'center', marginY: 2, fontWeight: "bold", color: COLORS.quiz.primary_text }}>Origin</Box>
                <Box sx={{ gridColumn: '9 / 10', textAlign: 'center', marginY: 2, fontWeight: "bold", color: COLORS.quiz.primary_text }}>Release</Box>
 
-               {/* Data Rows */}
-               {searchHistory.map((item) => <>
-                  <Box sx={{ gridColumn: '1 / 2', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}> {/* Center content */}
-                     <Box sx={{ maxWidth: "60px" }} component={"img"} src={getImgSrc(item.Name)}></Box>
-                  </Box>
-                  <Box sx={{ gridColumn: '2 / 3', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
-                     <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Name") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
-                        <Typography >{item.Name}</Typography>
-                     </Box>
-                  </Box>
-                  <Box sx={{ gridColumn: '3 / 4', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
-                     <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Sex") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
-                        <Typography >{item.Sex}</Typography>
-                     </Box>
-                  </Box>
-                  <Box sx={{ gridColumn: '4 / 5', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
-                     <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Age_Group") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
-                        <Typography >{item.Age_Group}</Typography>
-                        {checkValueDiff(checkAgeGroup(item.Age_Group) ?? 0, checkAgeGroup(targetChar?.Age_Group ?? "12-18") ?? 0)}
-                     </Box>
-                  </Box>
-                  <Box sx={{ gridColumn: '5 / 6', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}> {/* Center content */}
-                     <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Hair_Color") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
-                        <Typography >{item.Hair_Color}</Typography>
-                     </Box>
-                  </Box>
-                  <Box sx={{ gridColumn: '6 / 7', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
-                     <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Eye_Color") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
-                        <Typography >{item.Eye_Color}</Typography>
-                     </Box>
-                  </Box>
-                  <Box sx={{ gridColumn: '7 / 8', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
-                     <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Height") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
-                        <Typography >{item.Height}</Typography>
-                        {checkValueDiff(item.Height ?? 0, targetChar?.Height ?? 0)}
-                     </Box>
-                  </Box>
-                  <Box sx={{ gridColumn: '8 / 9', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
-                     <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Origin") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
-                        <Typography >{item.Origin}</Typography>
-                     </Box>
-                  </Box>
-                  <Box sx={{ gridColumn: '9 / 10', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
-                     <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("First_Release_Year") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
-                        <Typography >{item.First_Release_Year}</Typography>
-                        {checkValueDiff(item.First_Release_Year ?? 0, targetChar?.First_Release_Year ?? 0)}
-                     </Box>
-                  </Box>
-               </>)}
             </Box>
+            {/* Data Rows */}
+            {searchHistory.map((item) => <Box key={item.Name} sx={{ display: 'grid', gridTemplateColumns: '60px repeat(8, 1fr)', gap: 2, paddingX: 2, marginBottom: 2 }}>
+               <Box sx={{ gridColumn: '1 / 2', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}> {/* Center content */}
+                  <Box sx={{ maxWidth: "60px" }} component={"img"} src={getImgSrc(item.Name)}></Box>
+               </Box>
+               <Box sx={{ gridColumn: '2 / 3', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
+                  <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Name") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
+                     <Typography >{item.Name}</Typography>
+                  </Box>
+               </Box>
+               <Box sx={{ gridColumn: '3 / 4', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
+                  <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Sex") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
+                     <Typography >{item.Sex}</Typography>
+                  </Box>
+               </Box>
+               <Box sx={{ gridColumn: '4 / 5', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
+                  <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Age_Group") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
+                     <Typography >{item.Age_Group}</Typography>
+                     {checkValueDiff(checkAgeGroup(item.Age_Group) ?? 0, checkAgeGroup(targetChar?.Age_Group ?? "12-18") ?? 0)}
+                  </Box>
+               </Box>
+               <Box sx={{ gridColumn: '5 / 6', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}> {/* Center content */}
+                  <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Hair_Color") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
+                     <Typography >{item.Hair_Color}</Typography>
+                  </Box>
+               </Box>
+               <Box sx={{ gridColumn: '6 / 7', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
+                  <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Eye_Color") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
+                     <Typography >{item.Eye_Color}</Typography>
+                  </Box>
+               </Box>
+               <Box sx={{ gridColumn: '7 / 8', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
+                  <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Height") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
+                     <Typography >{item.Height}</Typography>
+                     {checkValueDiff(item.Height ?? 0, targetChar?.Height ?? 0)}
+                  </Box>
+               </Box>
+               <Box sx={{ gridColumn: '8 / 9', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
+                  <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("Origin") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
+                     <Typography >{item.Origin}</Typography>
+                  </Box>
+               </Box>
+               <Box sx={{ gridColumn: '9 / 10', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: 50 }}>
+                  <Box sx={{ width: "100%", flexGrow: 1, display: "flex", alignItems: "center", padding: "10px", backgroundColor: item.ValidFields?.includes("First_Release_Year") ? COLORS.quiz.success : COLORS.quiz.main, height: "100%" }}>
+                     <Typography >{item.First_Release_Year}</Typography>
+                     {checkValueDiff(item.First_Release_Year ?? 0, targetChar?.First_Release_Year ?? 0)}
+                  </Box>
+               </Box>
+            </Box>)}
          </Box>
          {
 
