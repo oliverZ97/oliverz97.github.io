4407f7d5e91a10b8ca6ae28eb3db63fa3ede4232
Oliver Ziemann <oliver@mordoor.de>
2025-02-16 22:06:30 +0100

    wip multiple choice

---
+++ src/components/MultipleChoiceQuiz/MultipleChoiceQuiz.tsx
@@ -0,0 +1,262 @@
+import { Box, Button, CircularProgress, Typography } from "@mui/material";
+import { getImgSrc } from "common/quizUtils";
+import { Character } from "common/types";
+import { AnimeAutocomplete } from "components/AnimeAutocomplete";
+import { CharacterAutocomplete } from "components/CharacterAutocomplete";
+import { DayStreak, StreakRef } from "components/Streak";
+import { SyntheticEvent, useEffect, useRef, useState } from "react";
+import { COLORS } from "styling/constants";
+
+interface ImageCharacterQuizProps {
+  charData: Character[];
+  animeData: string[];
+  getRandomCharacter: () => Character;
+}
+
+interface ImageTarget {
+  character: string;
+  anime: string;
+  isTarget: boolean;
+}
+
+const BASEPOINTS_ANIME = 1000;
+const BASEPOINTS_CHAR = 1500;
+
+export default function MultipleChoiceQuiz({
+  charData,
+  getRandomCharacter,
+  animeData,
+}: ImageCharacterQuizProps) {
+  const [isSolving, setIsSolving] = useState(false);
+  const [answers, setAnswers] = useState<ImageTarget[]>([]);
+  const [target, setTarget] = useState<Character | null>(null);
+  const [score, setScore] = useState(0);
+  const [lifes, setLifes] = useState(3);
+  const [selectedAnswer, setSelectedAnswer] = useState<ImageTarget | null>(
+    {} as ImageTarget
+  );
+
+  const streakRef = useRef<StreakRef | null>(null);
+
+  useEffect(() => {
+    if (!target) {
+      resetTargets();
+    } else {
+      getTargetAnswers();
+    }
+  }, [target]);
+
+  useEffect(() => {
+    if (selectedAnswer) {
+      if (selectedAnswer.isTarget) {
+        calculatePoints();
+        setTimeout(() => {
+          resetTargets();
+        }, 1000);
+      } else {
+        setLifes(lifes - 1);
+      }
+    }
+  }, [selectedAnswer]);
+
+  function resetImageQuiz() {
+    setAnswers([
+      {
+        character: "",
+        anime: "",
+        isTarget: false,
+      },
+    ]);
+    resetTargets();
+    setScore(0);
+    setIsSolving(false);
+  }
+
+  function resetTargets() {
+    const targetCharacters = getRandomCharacterArray(1);
+    const targets = targetCharacters[0];
+    setSelectedAnswer(null);
+    setTarget(targets);
+  }
+
+  function getRandomCharacterArray(count: number) {
+    let counter = 0;
+    let chars: Character[] = [];
+    while (counter < Math.max(0, count)) {
+      const char = getRandomCharacter();
+      if (!chars.some((item) => item.Name === char.Name)) {
+        chars.push(char);
+        counter++;
+      }
+    }
+    return chars;
+  }
+
+  function getTargetAnswers() {
+    if (target) {
+      let answers: ImageTarget[] = [];
+      const correctAnswer: ImageTarget = {
+        anime: target.Anime,
+        character: target.Name,
+        isTarget: true,
+      };
+      answers.push(correctAnswer);
+
+      let randoms = getRandomCharacterArray(3);
+      randoms.forEach((item) => {
+        answers.push({
+          anime: item.Anime,
+          character: item.Name,
+          isTarget: false,
+        });
+      });
+      answers = shuffleArray(answers);
+      setAnswers(answers);
+    }
+  }
+
+  function shuffleArray(array: ImageTarget[]) {
+    for (let i = array.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  function checkCorrectAnswers(answer: ImageTarget) {
+    setSelectedAnswer(answer);
+  }
+
+  function calculatePoints() {
+    setScore(score + 1);
+  }
+
+  function returnAnswerColor(answer: ImageTarget | null) {
+    if (selectedAnswer) {
+      if (
+        selectedAnswer.character === answer?.character &&
+        selectedAnswer.isTarget
+      ) {
+        return COLORS.quiz.success;
+      } else {
+        return COLORS.quiz.failed;
+      }
+    } else {
+      return COLORS.quiz.main;
+    }
+  }
+
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        backgroundColor: COLORS.quiz.secondary,
+        padding: 4,
+        borderRadius: "16px",
+        border: `1px solid ${COLORS.quiz.light}`,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "500px",
+      }}
+    >
+      <DayStreak
+        ref={streakRef}
+        streakKey={"imageStreak"}
+        colorRotate="250deg"
+        sx={{ top: "-121px", right: "-1px" }}
+      ></DayStreak>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 2,
+        }}
+      >
+        <Typography>{score}</Typography>
+        <Box sx={{ display: "flex", gap: 4 }}>
+          {target && (
+            <Box
+              key={target.Name}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                gap: 2,
+              }}
+            >
+              <Box
+                width={"200px"}
+                component={"img"}
+                src={getImgSrc(target.Name)}
+              ></Box>
+            </Box>
+          )}
+          {!target && (
+            <Box sx={{ height: "275.33px", width: "200px" }}>
+              <CircularProgress size="30px" color="primary"></CircularProgress>
+            </Box>
+          )}
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexWrap: "wrap",
+            width: "70%",
+            gap: 2,
+          }}
+        >
+          {target &&
+            answers.map((answer) => (
+              <Box
+                sx={{
+                  border: `1px solid ${COLORS.quiz.light}`,
+                  width: "450px",
+                  paddingX: 2,
+                  paddingY: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  color: "white",
+                  cursor: "pointer",
+                  backgroundColor: returnAnswerColor(answer),
+                }}
+                onClick={() => checkCorrectAnswers(answer)}
+              >
+                <Typography fontWeight={"bold"}>{answer.character}</Typography>
+                <Typography>{answer.anime}</Typography>
+              </Box>
+            ))}
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: 4,
+          width: "100%",
+        }}
+      >
+        <Button
+          sx={{
+            color: COLORS.quiz.light,
+            borderColor: COLORS.quiz.light,
+            "&:hover": {
+              fontWeight: "bold",
+              borderColor: COLORS.quiz.tertiary,
+            },
+          }}
+          variant="outlined"
+          onClick={resetImageQuiz}
+        >
+          Reset
+        </Button>
+        {/* {isSolving && <Typography fontSize={"24px"}>🏆 {score}</Typography>} */}
+      </Box>
+    </Box>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,145 +3,207 @@ import { useEffect, useState } from "react";
 import characterData from "data/character_data.json";
 import { COLORS } from "styling/constants";
 
-import bg from "assets/bg.jpg"
+import bg from "assets/bg.jpg";
 import { Character } from "common/types";
 import BasicCharacterQuiz from "components/BasicCharacterQuiz/BasicCharacterQuiz";
 import ImageCharacterQuiz from "components/ImageCharacterQuiz/ImageCharacterQuiz";
 import DrawerBasic from "components/CustomDrawer";
+import MultipleChoiceQuiz from "components/MultipleChoiceQuiz/MultipleChoiceQuiz";
 
 export interface Score {
-   points: number;
-   date: string;
+  points: number;
+  date: string;
 }
 
 const Home = () => {
-   const [charData, setCharData] = useState<Character[]>([]);
-   const [animeData, setAnimeData] = useState<string[]>([]);
+  const [charData, setCharData] = useState<Character[]>([]);
+  const [animeData, setAnimeData] = useState<string[]>([]);
 
-   useEffect(() => {
-      if (charData.length === 0) {
-         setCharData([...characterData.sort((a, b) => a.Name < b.Name ? -1 : 1)] as Character[])
-      }
-      if (animeData.length === 0) {
-         setAnimeData([...new Set(charData.map((item) => item.Anime))].sort((a, b) => a < b ? -1 : 1))
-      }
-   }, [charData, characterData, animeData]);
-
-   function getRandomCharacter() {
-      const charArray = Object.values(characterData);
-      let i = Math.floor(Math.random() * charArray.length);
-      const target = charArray[i];
-      return target as Character;
-   }
-
-
-
-   interface TabPanelProps {
-      children?: React.ReactNode;
-      index: number;
-      value: number;
-   }
-
-
-   function CustomTabPanel(props: TabPanelProps) {
-      const { children, value, index, ...other } = props;
-
-      return (
-         <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-         >
-            {value === index && <Box sx={{ paddingY: 3 }}>{children}</Box>}
-         </div>
+  useEffect(() => {
+    if (charData.length === 0) {
+      setCharData([
+        ...characterData.sort((a, b) => (a.Name < b.Name ? -1 : 1)),
+      ] as Character[]);
+    }
+    if (animeData.length === 0) {
+      setAnimeData(
+        [...new Set(charData.map((item) => item.Anime))].sort((a, b) =>
+          a < b ? -1 : 1
+        )
       );
-   }
+    }
+  }, [charData, characterData, animeData]);
 
-   function a11yProps(index: number) {
-      return {
-         id: `simple-tab-${index}`,
-         'aria-controls': `simple-tabpanel-${index}`,
-      };
-   }
+  function getRandomCharacter() {
+    const charArray = Object.values(characterData);
+    let i = Math.floor(Math.random() * charArray.length);
+    const target = charArray[i];
+    return target as Character;
+  }
 
-   const [value, setValue] = useState(0);
+  interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+  }
 
-   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-      setValue(newValue);
-   };
+  function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
 
-   return (
-      <>
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && <Box sx={{ paddingY: 3 }}>{children}</Box>}
+      </div>
+    );
+  }
 
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
 
-         <Box sx={{
-            backgroundColor: COLORS.quiz.background,
-            background: `url(${bg})`,
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            maxWidth: "100%",
-            minHeight: "100vh",
-            position: "relative"
-         }}>
+  const [value, setValue] = useState(0);
 
-            <DrawerBasic>
-               <Typography sx={{ fontSize: "24px", fontWeight: "bold", marginBottom: 2, color: COLORS.quiz.primary_text }}>Current Anime Index</Typography>
-               {animeData.map((item) => <Typography key={item} fontSize={"12px"} color={COLORS.quiz.primary_text}>{item}</Typography>)}
-            </DrawerBasic>
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
-            <Box sx={{
-               display: "flex",
-               flexDirection: "column",
-               justifyContent: "center",
-               alignItems: "center",
-            }}>
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: COLORS.quiz.background,
+          background: `url(${bg})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          maxWidth: "100%",
+          minHeight: "100vh",
+          position: "relative",
+        }}
+      >
+        <DrawerBasic>
+          <Typography
+            sx={{
+              fontSize: "24px",
+              fontWeight: "bold",
+              marginBottom: 2,
+              color: COLORS.quiz.primary_text,
+            }}
+          >
+            Current Anime Index
+          </Typography>
+          {animeData.map((item) => (
+            <Typography
+              key={item}
+              fontSize={"12px"}
+              color={COLORS.quiz.primary_text}
+            >
+              {item}
+            </Typography>
+          ))}
+        </DrawerBasic>
 
-
-               <Box sx={{ marginTop: "100px", width: "80%", backgroundColor: COLORS.quiz.secondary, borderRadius: "16px", border: `1px solid ${COLORS.quiz.light}`, marginBottom: 4 }}>
-                  <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-
-                     <Box sx={{ paddingLeft: 2 }}>
-                        <Tabs sx={{
-
-                           '& .MuiTabs-indicator': {
-                              backgroundColor: COLORS.quiz.light,
-                           },
-                        }} value={value} onChange={handleChange} aria-label="basic tabs example">
-                           <Tab sx={{
-                              color: COLORS.quiz.light,
-                              "&.Mui-selected": {
-                                 color: "white",
-                              },
-                           }}
-                              label="Character Quiz" {...a11yProps(0)} />
-                           <Tab sx={{
-                              "&.Mui-selected": {
-                                 color: "white",
-                              },
-                           }}
-                              label="Character Image Quiz" {...a11yProps(1)} />
-                        </Tabs>
-                     </Box>
-
-                  </Box>
-               </Box>
-               <Box sx={{ width: "80%" }}>
-                  <CustomTabPanel value={value} index={0}>
-                     <BasicCharacterQuiz charData={charData} getRandomCharacter={getRandomCharacter}></BasicCharacterQuiz>
-                  </CustomTabPanel>
-
-                  <CustomTabPanel value={value} index={1}>
-                     <ImageCharacterQuiz animeData={animeData} charData={charData} getRandomCharacter={getRandomCharacter}></ImageCharacterQuiz>
-                  </CustomTabPanel>
-               </Box>
-
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              marginTop: "100px",
+              width: "80%",
+              backgroundColor: COLORS.quiz.secondary,
+              borderRadius: "16px",
+              border: `1px solid ${COLORS.quiz.light}`,
+              marginBottom: 4,
+            }}
+          >
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Box sx={{ paddingLeft: 2 }}>
+                <Tabs
+                  sx={{
+                    "& .MuiTabs-indicator": {
+                      backgroundColor: COLORS.quiz.light,
+                    },
+                  }}
+                  value={value}
+                  onChange={handleChange}
+                  aria-label="basic tabs example"
+                >
+                  <Tab
+                    sx={{
+                      color: COLORS.quiz.light,
+                      "&.Mui-selected": {
+                        color: "white",
+                      },
+                    }}
+                    label="Character Quiz"
+                    {...a11yProps(0)}
+                  />
+                  <Tab
+                    sx={{
+                      color: COLORS.quiz.light,
+                      "&.Mui-selected": {
+                        color: "white",
+                      },
+                    }}
+                    label="Multiple Choice Quiz"
+                    {...a11yProps(1)}
+                  />
+                  <Tab
+                    sx={{
+                      color: COLORS.quiz.light,
+                      "&.Mui-selected": {
+                        color: "white",
+                      },
+                    }}
+                    label="Multiple Choice Quiz"
+                    {...a11yProps(2)}
+                  />
+                </Tabs>
+              </Box>
             </Box>
-         </Box >
-      </>
-   );
+          </Box>
+          <Box sx={{ width: "80%" }}>
+            <CustomTabPanel value={value} index={0}>
+              <BasicCharacterQuiz
+                charData={charData}
+                getRandomCharacter={getRandomCharacter}
+              ></BasicCharacterQuiz>
+            </CustomTabPanel>
+
+            <CustomTabPanel value={value} index={1}>
+              <ImageCharacterQuiz
+                animeData={animeData}
+                charData={charData}
+                getRandomCharacter={getRandomCharacter}
+              ></ImageCharacterQuiz>
+            </CustomTabPanel>
+
+            <CustomTabPanel value={value} index={2}>
+              <MultipleChoiceQuiz
+                animeData={animeData}
+                charData={charData}
+                getRandomCharacter={getRandomCharacter}
+              ></MultipleChoiceQuiz>
+            </CustomTabPanel>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Home;
