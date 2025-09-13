13dc9ba4e4fd270c300c0122e58c708407a61ad3
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2025-09-13 15:01:49 +0200

    add ruby project

---
+++ backenime/db/migrate/20250913130223_create_character.rb
@@ -0,0 +1,21 @@
+class CreateCharacter < ActiveRecord::Migration[8.0]
+  def change
+    create_table :characters do |t|
+      t.string :label
+      t.string :name
+      t.string :sex
+      t.string :origin
+      t.string :hair_color
+      t.string :age
+      t.string :age_group
+      t.integer :height
+      t.string :eye_color
+      t.integer :anime_id
+      t.datetime :birthday
+      t.string :difficulty
+      t.string :editorial_staff_hint
+      t.integer :version
+      t.timestamps
+    end
+  end
+end
---
+++ backenime/db/migrate/20250913132002_create_genre.rb
@@ -0,0 +1,8 @@
+class CreateGenre < ActiveRecord::Migration[8.0]
+  def change
+    create_table :genres do |t|
+      t.string :name
+      t.timestamps
+    end
+  end
+end
