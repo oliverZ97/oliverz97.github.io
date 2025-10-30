7723c07be959fef49e8affa160d1a1a614811001
Oliver Ziemann <oliver@mordoor.de>
2025-10-30 10:13:16 +0100

    fix(quiz): :bug: BUG-7: Fix yesterdays character bug after version release

--- docs/assets/PageNotFound-37fe9693.js
+++
@@ -1,1 +0,0 @@
-import{j as t,Z as s}from"./index-e319dd0d.js";const e=()=>t.jsx(s,{children:"This path is not exist!"});export{e as default};
--- src/common/version.ts
+++ src/common/version.ts
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-export const VERSION = "1.7.9";
+export const VERSION = "1.7.10";
 
 interface CHAR_VERSION {
   version: string;
