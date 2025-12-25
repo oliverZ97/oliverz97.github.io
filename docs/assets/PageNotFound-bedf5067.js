1c201f0b940c3c3465ef5ccd242ad49c8c189952
Oliver Ziemann <oliver@mordoor.de>
2025-12-25 23:47:51 +0100

    feat(version): :rocket: v1.7.15: Christmas Update with new Higher Lower Gamemode

--- docs/assets/PageNotFound-bedf5067.js
+++
@@ -1,1 +0,0 @@
-import{j as t,a0 as s}from"./index-1eec5db0.js";const a=()=>t.jsx(s,{children:"This path is not exist!"});export{a as default};
--- src/common/version.ts
+++ src/common/version.ts
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-export const VERSION = "1.7.14";
+export const VERSION = "1.7.15";
 
 interface CHAR_VERSION {
   version: string;
