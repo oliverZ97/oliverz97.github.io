3410d1388cb3a95fa744a06e2a24ba8092f13ab0
Oliver Ziemann <oliver@mordoor.de>
2026-02-23 14:32:54 +0100

    build

---
+++ docs/assets/PageNotFound-1b8b781e.js
@@ -0,0 +1,1 @@
+import{j as t,N as s}from"./index-e0b60eca.js";const e=()=>t.jsx(s,{children:"This path is not exist!"});export{e as default};
--- docs/assets/PageNotFound-948880b5.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as s}from"./index-3f9b748e.js";const e=()=>t.jsx(s,{children:"This path is not exist!"});export{e as default};
