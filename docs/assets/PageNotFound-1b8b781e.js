c02d1b42f42ea05a0265bd8c67dc00e7b9ec14de
Oliver Ziemann <oliver@mordoor.de>
2026-04-06 11:14:53 +0200

    feat(version):

--- docs/assets/PageNotFound-1b8b781e.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as s}from"./index-e0b60eca.js";const e=()=>t.jsx(s,{children:"This path is not exist!"});export{e as default};
---
+++ docs/assets/PageNotFound-58e2cdb7.js
@@ -0,0 +1,1 @@
+import{j as t,N as s}from"./index-488cdca7.js";const e=()=>t.jsx(s,{children:"This path is not exist!"});export{e as default};
